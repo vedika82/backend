981c3618c4f9bf1aa72613d2a42d8626c2f5bb7a
vedika82 <anandvedika420@gmail.com>
2026-02-02 22:44:37 +0530

    seeing expressRouting file

--- CD220Labs/expressjs/expressRouterMiddleware.js
+++ CD220Labs/expressjs/expressRouterMiddleware.js
@@ -1,3 +1,6 @@
+// This server branches and the requests based on the end points and uses routers to handle them. All the /user endpoints are handled by userRouter and /item endpoints are handled by itemRouter.
+
+
 const express = require('express');
 const app = new express();
 
@@ -27,3 +30,13 @@ app.listen(3333, () => {
     console.log(`Listening at http://localhost:3333`)
 })
 
+
+//INSTRUCTIONS:
+// You should see output which says Listening at http://localhost:3333.
+// In the second terminal window, use the following curl command.
+// curl localhost:3333/item/1
+// You should see output which says Item 1 last enquiry Fri Nov 20 2020 15:17:46 GMT+0530 (India Standard Time).
+
+// In the second terminal window, use the following curl command.
+// curl localhost:3333/user/1
+// You should see output which says User 1 last successful login Fri Nov 20 2020 15:19:52 GMT+0530 (India Standard Time).
